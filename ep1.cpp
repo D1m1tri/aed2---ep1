f3ae15bd1b04f550e0fc9f780a37ae5727c69e09
Diego Pedroso <diego.pedroso15@gmail.com>
2023-05-31 16:32:49 -0300

    fix

--- ep1.cpp
+++ ep1.cpp
@@ -1,26 +1,26 @@
-// Ep1 realizado por D1m1tri
+ // Ep1 realizado por D1m1tri
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
-
+#include <stdbool.h>
 #include <limits.h>
 
-// elemento das listas de adjacência e de resposta - NAO ALTERE ESTA DEFINICAO
-typedef struct estr 
+// elemento das listas de adjacÃªncia e de resposta - NAO ALTERE ESTA DEFINICAO
+typedef struct estr
 {
   int adj; // elemento
-  int peso; // custo (não precisa ser usado na resposta)
+  int peso; // custo (nÃ£o precisa ser usado na resposta)
   struct estr *prox;
 } NO;
 
 // vertices do grafo (salas) - use este tipo ao criar o grafo  - NAO ALTERE ESTA DEFINICAO
-typedef struct 
+typedef struct
 {
   int flag; // para uso na busca em largura e profundidade, se necessario
   bool aberto; // vale true se a sala em questao esta aberta
   int via; // use este campo se precisar computar algum caminho etc.
-  int dist; // use este campo se precisar computar alguma distância etc.
+  int dist; // use este campo se precisar computar alguma distÃ¢ncia etc.
   NO* inicio;
 } VERTICE;
 
@@ -33,7 +33,7 @@ NO *caminho(int N, int A, int *ijpeso, int *aberto, int inicio, int fim, int cha
 // necessitar
 //------------------------------------------
 
-// função para criar arestas
+// funÃ§Ã£o para criar arestas
 void criarAresta(VERTICE* grafo, int anterior, int proximo, int peso){
   NO* temp = grafo[anterior].inicio;
   grafo[anterior].inicio = (NO*) malloc(sizeof(NO));
@@ -54,6 +54,7 @@ VERTICE* criarGrafo(int N, int A, int * ijpeso, int * aberto){
   VERTICE * grafo = (VERTICE *) malloc(N * sizeof(VERTICE));
   for (int i=0; i<N;i++) {
     grafo[i].aberto = aberto[i];
+    grafo[i].inicio = NULL;
   }
   int i=0;
   while (i<A*3) {
@@ -63,7 +64,7 @@ VERTICE* criarGrafo(int N, int A, int * ijpeso, int * aberto){
   return grafo;
 }
 
-// função para inicializar o grafo
+// funÃ§Ã£o para inicializar o grafo
 void inicializarGrafo(VERTICE * grafo, int * distancia, int * anterior, int inicio, int tamanho){
   for (int v=0; v<tamanho; v++){
     distancia[v] = INT_MAX/2;
@@ -72,7 +73,7 @@ void inicializarGrafo(VERTICE * grafo, int * distancia, int * anterior, int inic
   distancia[inicio] = 0;
 }
 
-// função para relaxar a aresta
+// funÃ§Ã£o para relaxar a aresta
 void relaxa(VERTICE * grafo, int * distancia, int * anterior, int verticeA, int verticeB){
   NO* no = grafo[verticeA].inicio;
   while (no && no->adj != verticeB) {
@@ -86,7 +87,7 @@ void relaxa(VERTICE * grafo, int * distancia, int * anterior, int verticeA, int 
   }
 }
 
-// função para verificar a existência de vertices em aberto
+// funÃ§Ã£o para verificar a existÃªncia de vertices em aberto
 bool existeAberto(bool * aberto, int N){
   for (int i=0; i<N; i++) {
     if (aberto[i]) return true;
@@ -94,7 +95,7 @@ bool existeAberto(bool * aberto, int N){
   return false;
 }
 
-// busca o primeiro nó aberto
+// busca o primeiro nÃ³ aberto
 int menorDist(int N, bool * aberto, int * distancia){
   int i;
   for (i=0; i<N; i++) {
@@ -118,6 +119,7 @@ int menorDist(int N, bool * aberto, int * distancia){
 int * dijkstra(VERTICE * grafo, int N, int inicio, int * fechado, bool bloquearPassagem){
   int * distancia = (int*) malloc(N*sizeof(int));
   int * anterior = (int *) malloc(N*sizeof(int));
+
   inicializarGrafo(grafo, distancia, anterior, inicio, N);
   bool aberto[N];
   for (int i=0; i<N; i++) {
@@ -128,6 +130,7 @@ int * dijkstra(VERTICE * grafo, int N, int inicio, int * fechado, bool bloquearP
     int u = menorDist(N, aberto, distancia);
     aberto[u] = false;
     NO * no = grafo[u].inicio;
+
     while (no) {
       if (bloquearPassagem){
         if (fechado[u] == 1) {
@@ -176,7 +179,7 @@ void printAll(int* parcial, int N){
   }
 }
 
-// função para comparar dois resultados, escolher o melhor e retornar o array de NO
+// funÃ§Ã£o para comparar dois resultados, escolher o melhor e retornar o array de NO
 NO * comparar(int * parcial, int * parcialComChave, int fim, int chave){
   NO * temp;
   temp = NULL;
@@ -219,44 +222,29 @@ NO * comparar(int * parcial, int * parcialComChave, int fim, int chave){
 NO * caminho(int N, int A, int *ijpeso, int *aberto, int inicio, int fim, int chave)
 {
   VERTICE* grafo = criarGrafo(N, A, ijpeso, aberto);
-  NO* resp = comparar(dijkstra(grafo, N, inicio-1, aberto, true), dijkstra(grafo, N, chave-1, aberto, false), fim, chave);
 
-  return resp;
-}
-
-
-
-int main() {
-
-
-  // aqui vc pode incluir codigo de teste
-
-  // exemplo de teste trivial
-
-  int N=9; // grafo de 9 vértices numerados de 1..9
-  int A=10; // grafo de 10 arestas
-  int aberto[] = {0,1,1,1,1,1,1,1,1}; // vértice 1 bloqueado
-  int inicio=7;
-  int fim=4;
-  int chave=6;
-  int ijpeso[]={1,2,30, 1,3,20, 2,6,20, 2,7,30, 3,7,80, 5,8,10, 6,7,10, 7,9,80, 4,3,20, 4,9,80};
-
-  // o EP sera testado com uma serie de chamadas como esta
-  NO * teste = NULL;
-  teste = caminho(N, A, ijpeso, aberto, inicio, fim, chave);
-  printf("Caminho = [");
-  if (teste) {
-    while (teste) {
-      printf(" %i,", teste->adj);
-      teste=teste->prox;
-    }
+  if (chave > 0) {
+    NO* resp = comparar(dijkstra(grafo, N, inicio-1, aberto, true), dijkstra(grafo, N, chave-1, aberto, false), fim, chave);
+    return resp;
   }
   else {
-    printf(" NULL");
+    int *parcial = dijkstra(grafo, N, inicio-1, aberto, true);
+    NO * temp;
+    temp = NULL;
+    int atual = fim-1;
+    while (atual != -1) {
+      NO * novo = (NO*) malloc(sizeof(NO));
+      novo->adj = atual+1;
+      novo->peso = parcial[atual*3+1];
+      novo->prox = temp;
+      temp = novo;
+      atual = parcial[atual*3+2];
+    }
+    if (temp->prox == NULL) {
+      return NULL;
+    }
+    return temp;
   }
-  printf(" ]\n");
-  return 0;
 }
 
-// por favor nao inclua nenhum código abaixo da função main()
-
+//////////////////////////////////////////////////////////////////////////////
